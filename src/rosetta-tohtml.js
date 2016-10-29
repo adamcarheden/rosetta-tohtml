87838d7c2148501334a9ccf74a4fa7453e7f687e
Adam Carheden <adam.carheden@gmail.com>
2016-10-29 00:49:45 -0600

    Fixed bug that prevented multipel tags for working right

--- src/rosetta-tohtml.js
+++ src/rosetta-tohtml.js
@@ -1,31 +1,44 @@
-/*
-* TODO: 
-* fork rosetta and add an `npm init` it so we can depend on it
-* Generalize to work for anything that can dump html
-* put markdown specific stuff in another project
-* webpack
-* karma
-* nightwatch
-* plunker
-*/
-(function () {
+"use strict";
+var rosetta = require('rosetta.js');
 
-  if (!window.rosetta) { return; }
+function replaceElement(oldE, newE) {
+	for (var i=0; i<oldE.attributes.length; i++)
+		newE.setAttribute(oldE.attributes[i].name, oldE.attributes[i].value);
+	// This doesn't work because rosetta processes the output of 
+	// getElementsByTagName('script'), which is a NodeList, which is a
+	// live collection. So when we replace a <script> with a different tag,
+	// we break index in the NodeList rosetta is using.
+	//
+	//oldE.parentNode.replaceChild(newE,oldE);
+	//
+	// This works fine instead...
+	oldE.parentNode.insertBefore(newE, oldE);
+	// Unless the user were to generate a <script> tag.
+	// Bad user! No cookie.
+	// (Really it should be a rosetta bug, but generating 
+	//  more script tags is likely going to break things in other ways too.)
+}
 
-	function replaceElement(oldE, newE) {
-		for (var i=0; i<oldE.attributes.length; i++)
-			newE.setAttribute(oldE.attributes[i].name, oldE.attributes[i].value);
-		oldE.parentNode.replaceChild(newE,oldE);
-	}
+var RFUNNAME = 'ROSETTA-HTML-REPLACEMENT-FUNCTIONS';
+function createECMASrc (src, compiler) {
+	var newE = document.createElement('div');
+	newE.innerHTML = compiler(src)
 
-  function createECMASrc (md, scrTag) {
-		var newE = document.createElement('div');
-		newE.innerHTML = marked(md);
-		window.rosetta.md = function(scrElem) { replaceElement(scrElem, newE); }
-		return 'window.rosetta.md(document.currentScript)';
-  }
+	if (typeof(window) === undefined) throw new Error('It doesn\'t make sense to use rosetta or rosetta-html outside of a browser');
+	
+	if (!(RFUNNAME in window)) window[RFUNNAME] = [];
+	window[RFUNNAME].push(function(scrElem) { replaceElement(scrElem, newE); });
+	return 'window["'+RFUNNAME+'"]['+(window[RFUNNAME].length - 1)+'](document.currentScript);';
+}
 
-  rosetta.appendCompiler([ "text/markdown", "text/md" ], createECMASrc);
+var appendCompiler = function(mimeTypes, compiler) {
+	rosetta.appendCompiler(mimeTypes, function(src) {
+		return createECMASrc(src, compiler);
+	});
+}
 
-})();
-rosetta.translateAll();
+module.exports = {
+	rosetta: rosetta,
+	appendCompiler: appendCompiler,
+	translateAll: function() { rosetta.translateAll(); },
+};
